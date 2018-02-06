80329653e4b96335c3b168ce019a1bc8290648e0
TomKo1 <kottomasz98@gmail.com>
2018-02-06 12:28:44 +0100

    Done to the 14.2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,7 +75,16 @@ test "associated microposts should be destroyed" do
 end
 
 
-
+test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 
 
 
